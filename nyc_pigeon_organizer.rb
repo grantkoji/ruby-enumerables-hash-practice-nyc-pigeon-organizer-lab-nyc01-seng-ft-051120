407e109463d448f6b42cca83f23fd2797faf3404
Grant Yoshitsu <grantkoji@gmail.com>
2020-05-06 23:17:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,6 @@ def nyc_pigeon_organizer(data)
     value_hash = value
     attribute_var = attribute
     value_hash.each do |category, name|
-binding.pry
       if reorganized_data[name][attribute_var]
         reorganized_data[name][attribute_var] << category
       else
